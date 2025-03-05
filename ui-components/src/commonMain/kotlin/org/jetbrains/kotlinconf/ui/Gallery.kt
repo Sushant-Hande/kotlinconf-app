587d1fe9db21e0be1b9f082456f501d10e20f1ce
Victor Kropp <victor.kropp@jetbrains.com>
2025-03-05 13:36:15 +0100

    add PartnerCard component

--- ui-components/src/commonMain/kotlin/org/jetbrains/kotlinconf/ui/Gallery.kt
+++ ui-components/src/commonMain/kotlin/org/jetbrains/kotlinconf/ui/Gallery.kt
@@ -38,6 +38,7 @@ fun GalleryApp() {
                 PageMenuItemPreview()
                 PageTitlePreview()
                 ParagraphTitlePreview()
+                PartnerCardPreview()
                 ScrollIndicatorPreview()
                 SectionTitlePreview()
                 ServiceEventsPreview()
